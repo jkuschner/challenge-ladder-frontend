7f5a6a6c767bd64107b2082954a2bfc774604608
Joshua <jkuschne@ucsd.edu>
2025-02-10 08:39:06 -0800

    Implement MatchList.js, closes #005

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import logo from '../resources/logo.svg';
 import PlayerList from './PlayerList';
+import MatchList from './MatchList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <PlayerList/>
+      <MatchList/>
     </div>
   );
 }
---
+++ src/components/MatchList.js
@@ -0,0 +1,58 @@
+import { Temporal } from '@js-temporal/polyfill';
+import React, {useState, useEffect} from 'react';
+import MatchService from '../services/MatchService';
+import PlayerService from '../services/PlayerService';
+
+function MatchList() {
+    const [matches, setMatches] = useState([]);
+    const [players, setPlayers] = useState([]);
+
+    useEffect(() => {
+        MatchService.findAll()
+            .then(response => { setMatches(response.data); })
+            .catch(error => {
+                console.error("Error fetching(findAll) matches: ", error);
+            });
+    }, []);
+
+    useEffect(() => {
+        PlayerService.findAll()
+            .then(response => { setPlayers(response.data); })
+            .catch(error => {
+                console.error("Error fetching(findAll) players for id:player mappings", error);
+            });
+    }, []);
+
+    const playerIdMap = new Map();
+    players.forEach(player => {
+        playerIdMap.set(player.id, player.name);
+    })
+
+    return (
+        <div>
+            <h1>Matches</h1>
+            <table>
+            <thead>
+                <tr>
+                    <th>Players</th>
+                    <th>Result</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                {matches.map(match => (
+                    <tr>
+                        <td>
+                            {playerIdMap.get(match.player1)} vs. {playerIdMap.get(match.player2)}
+                        </td>
+                        <td>{match.score}</td>
+                        <td>{Temporal.PlainDate.from(match.matchDate).toString()}</td>
+                    </tr>
+                ))}
+            </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default MatchList;
